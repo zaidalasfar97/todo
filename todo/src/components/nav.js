5ef471a9e9bbd1c5a8665dad071daefba7660175
zaidalasfar97 <z.alasfar@hotmail.com>
2021-05-16 18:46:45 +0300

    finish

---
+++ todo/src/components/nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from 'react-bootstrap/Navbar';
+
+const Navgaiton = () => {
+  return (
+    <Navbar bg="primary" variant="dark">
+      <Navbar.Brand href="#home">
+        Home
+    </Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default Navgaiton;
