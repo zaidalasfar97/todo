395078538de83e31c27af5d6249044f05648a4cd
zaidalasfar97 <z.alasfar@hotmail.com>
2021-05-19 14:56:43 +0300

    done

--- src/app.js
+++ src/app.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ToDo from './components/todo/todo.js';
 import NavBar from './components/todo/navbar.js';
-// import Main from './components/todo/main.js';
-
-// import SortProvider from './components/context/sort';
 import PaginationProvider from './components/context/pagination';
 
 const App = () => (
   <>
     <NavBar />
-    {/* <SortProvider> */}
     <PaginationProvider>
       <ToDo />
     </PaginationProvider>
-    {/* </SortProvider> */}
   </>
 );
 
